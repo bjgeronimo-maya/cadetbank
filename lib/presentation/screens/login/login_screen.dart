c338414b4ca37b04c747723e9399ef4cd3bca94f
Aira Nicole Natividad <aira.natividad@paymaya.com>
2026-02-05 14:50:43 +0800

    homescreen activity

--- lib/presentation/screens/login/login_screen.dart
+++ lib/presentation/screens/login/login_screen.dart
@@ -1,8 +1,13 @@
 import 'package:cadetbank/core/res/values/dimens.dart';
 import 'package:cadetbank/core/res/values/strings.dart';
+import 'package:cadetbank/presentation/screens/login/cubits/login_form/login_form_cubit.dart';
 import 'package:cadetbank/presentation/screens/login/widgets/login_form.dart';
 import 'package:cadetbank/presentation/screens/login/widgets/login_logo.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:cadetbank/presentation/screens/home/home_screen.dart';
+import 'package:cadetbank/core/navigation/routes.dart';
+
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -28,7 +33,9 @@ class LoginScreen extends StatelessWidget {
     bottomNavigationBar: Padding(
       padding: const EdgeInsets.all(Dimens.s20),
       child:  ElevatedButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.of(context).pushNamed(Routes.home);
+        },
         child: Text(
           Strings.login,
           style: Theme.of(context).textTheme.labelMedium,
