846143deab7038cdc7045df09294b3eaea4609f4
Angelo <angelo.cruz@maya.ph>
2026-02-05 16:36:57 +0800

    feat: polished bottom navbar

--- lib/presentation/screens/registration/registration_screen.dart
+++ lib/presentation/screens/registration/registration_screen.dart
@@ -9,72 +9,54 @@ class RegistrationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: SafeArea(
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: Dimens.s20),
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    RichText(
-                      text: const TextSpan(
-                        style: TextStyle(
-                          fontSize: 50,
-                          fontWeight: FontWeight.w900,
-                        ),
-                        children: <TextSpan>[
-                          TextSpan(
-                            text: 'Start an \n',
-                            style: TextStyle(color: Colors.black),
-                          ),
-                          TextSpan(
-                            text: 'account',
-                            style: TextStyle(color: Colors.green),
-                          ),
-                        ],
-                      ),
+    body: SafeArea(
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Dimens.s20),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                RichText(
+                  text: const TextSpan(
+                    style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.w900,
                     ),
-
-                    const SizedBox(height: Dimens.s25),
-
-                    const RegistrationForm(),
-                  ],
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'Start an \n',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      TextSpan(
+                        text: 'account',
+                        style: TextStyle(color: Colors.green),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+
+                const SizedBox(height: Dimens.s25),
+
+                const RegistrationForm(),
+              ],
             ),
           ),
         ),
-        bottomNavigationBar: Padding(
-          padding: const EdgeInsets.all(Dimens.s20),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size.fromHeight(10),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                ),
-                child: const Text(Strings.kContinue),
-              ),
-              const SizedBox(height: Dimens.s20),
-              TextButton(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, Routes.login);
-                },
-                child: Text(
-                  Strings.login,
-                  style: Theme.of(context).textTheme.labelMedium?.copyWith(
-                        color: Colors.green,
-                      ),
-                ),
-              ),
-            ],
-          ),
+      ),
+    ),
+    bottomNavigationBar: Padding(
+      padding: const EdgeInsets.all(Dimens.s20),
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pushReplacementNamed(context, Routes.login);
+        },
+        child: Text(
+          Strings.kContinue,
+          style: Theme.of(context).textTheme.labelMedium,
         ),
-      );
+      ),
+    ),
+  );
 }
