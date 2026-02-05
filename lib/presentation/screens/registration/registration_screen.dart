28b101c97f6b9edd68c7f45c220560ad82b23852
Angelo <angelo.cruz@maya.ph>
2026-02-05 16:24:03 +0800

    feat: revised registration page UI

--- lib/presentation/screens/registration/registration_screen.dart
+++ lib/presentation/screens/registration/registration_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:cadetbank/core/navigation/routes.dart';
 import 'package:cadetbank/core/res/values/dimens.dart';
 import 'package:cadetbank/core/res/values/strings.dart';
@@ -10,54 +9,72 @@ class RegistrationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    body: Center(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: Dimens.s20),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            RichText(
-              text: const TextSpan(
-                style: TextStyle(color: Colors.black),
-                children: <TextSpan>[
-                  TextSpan(
-                      text: 'Start an '
-                  ),
-                  TextSpan(
-                    style: TextStyle(color: Colors.green),
-                    text: 'account',
-                  ),
-                ],
+        body: SafeArea(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: Dimens.s20),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    RichText(
+                      text: const TextSpan(
+                        style: TextStyle(
+                          fontSize: 50,
+                          fontWeight: FontWeight.w900,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'Start an \n',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          TextSpan(
+                            text: 'account',
+                            style: TextStyle(color: Colors.green),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    const SizedBox(height: Dimens.s25),
+
+                    const RegistrationForm(),
+                  ],
+                ),
               ),
             ),
-
-            const SizedBox(height: Dimens.s20),
-
-            const RegistrationForm(),
-          ],
-        ),
-      ),
-    ),
-    bottomNavigationBar: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text(Strings.kContinue),
-        ),
-
-        const SizedBox(height: Dimens.s20),
-
-        ElevatedButton(
-          onPressed: () {
-            Navigator.pushReplacementNamed(context, Routes.login);
-          },
-          child: Text(
-            Strings.login,
-            style: Theme.of(context).textTheme.labelMedium,
           ),
-        )
-      ],
-    )
-  );
+        ),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(Dimens.s20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(10),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                child: const Text(Strings.kContinue),
+              ),
+              const SizedBox(height: Dimens.s20),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, Routes.login);
+                },
+                child: Text(
+                  Strings.login,
+                  style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                        color: Colors.green,
+                      ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
 }
--- lib/presentation/screens/registration/widgets/registration_form.dart
+++ lib/presentation/screens/registration/widgets/registration_form.dart
@@ -1,31 +1,55 @@
+import 'package:cadetbank/core/res/values/dimens.dart';
 import 'package:cadetbank/core/res/values/strings.dart';
+import 'package:cadetbank/presentation/widgets/input_field.dart';
 import 'package:flutter/material.dart';
 
 class RegistrationForm extends StatelessWidget {
   const RegistrationForm({super.key});
 
   @override
-  Widget build(BuildContext context) => const Column(
-    children: [
-      TextField(
-        keyboardType: TextInputType.name,
-        decoration: InputDecoration(hintText: Strings.nameHint),
-      ),
+  Widget build(BuildContext context) => Column(
+        children: [
+          InputField(
+            label: 'Full Name',
+            hint: Strings.nameHint,
+            keyboardType: TextInputType.name,
+            onChanged: (value) {
+              // error handler for name change
+            },
+          ),
 
-      TextField(
-        keyboardType: TextInputType.emailAddress,
-        decoration: InputDecoration(hintText: Strings.emailHint),
-      ),
+          const SizedBox(height: Dimens.s16),
 
-      TextField(
-        keyboardType: TextInputType.number,
-        decoration: InputDecoration(hintText: Strings.mobileNumberHint),
-      ),
+          InputField(
+            label: 'Email Address',
+            hint: Strings.emailHint,
+            keyboardType: TextInputType.emailAddress,
+            onChanged: (value) {
+              // // error handler for email change
+            },
+          ),
 
-      TextField(
-        keyboardType: TextInputType.visiblePassword,
-        decoration: InputDecoration(hintText: Strings.passwordHint),
-      ),
-    ],
-  );
+          const SizedBox(height: Dimens.s16),
+
+          InputField(
+            label: 'Mobile Number',
+            hint: Strings.mobileNumberHint,
+            keyboardType: TextInputType.number,
+            onChanged: (value) {
+              // // error handler for number change
+            },
+          ),
+
+          const SizedBox(height: Dimens.s16),
+
+          InputField(
+            label: 'Password',
+            hint: Strings.passwordHint,
+            keyboardType: TextInputType.visiblePassword,
+            onChanged: (value) {
+              // // error handler for password change
+            },
+          ),
+        ],
+      );
 }
